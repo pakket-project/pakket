150153cc58c34550577a68e7bd7b51e1218dd67d
Sting Alleman <stingalleman@icloud.com>
2021-04-18 23:41:53 +0200

    feat(util/os): add removefolder method

--- util/os.go
+++ util/os.go
@@ -2,13 +2,15 @@ package util
 
 import (
 	"os"
+	"path"
 )
 
 var (
-	StewPath   = "/usr/local/stew"
-	ConfigFile = "/usr/local/stew/config.toml"
-	RepoPath   = "/usr/local/stew/repositories"
-	Binpath    = "/usr/local/bin"
+	StewPath     = "/usr/local/stew"
+	ConfigFile   = "/usr/local/stew/config.toml"
+	RepoPath     = "/usr/local/stew/repositories"
+	Binpath      = "/usr/local/bin"
+	TempRepoPath = "/var/tmp/stew/repo"
 )
 
 func DoesPathExist(path string) bool {
@@ -16,4 +18,32 @@ func DoesPathExist(path string) bool {
 	exists := os.IsNotExist(err)
 
 	return !exists
+}
+
+// First removes all files in the directory and then remove the directory itself.
+func RemoveFolder(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+
+	for _, name := range names {
+		err = os.RemoveAll(path.Join(dir, name))
+		if err != nil {
+			return err
+		}
+
+	}
+	err = os.Remove(dir)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
