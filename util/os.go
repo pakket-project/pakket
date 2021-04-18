26d2e7541698d929cc453a100ccece41872ba829
Sting Alleman <stingalleman@icloud.com>
2021-04-18 11:48:04 +0200

    feat(util/os): add doespathexist method

--- util/os.go
+++ util/os.go
@@ -1,7 +1,19 @@
 package util
 
+import (
+	"os"
+)
+
 var (
+	StewPath   = "/usr/local/stew"
 	ConfigFile = "/usr/local/stew/config.toml"
 	RepoPath   = "/usr/local/stew/repositories"
 	Binpath    = "/usr/local/bin"
 )
+
+func DoesPathExist(path string) bool {
+	_, err := os.Stat(path)
+	exists := os.IsNotExist(err)
+
+	return !exists
+}
