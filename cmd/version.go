e938f4e9869b26d0056a985364dbf2aa7c01fa46
Kees van Voorthuizen <me@keesvv.nl>
2021-04-19 15:04:28 +0200

    fix: add missing newline & use path.Join

--- cmd/version.go
+++ cmd/version.go
@@ -14,6 +14,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of Stew",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Stew Package Manager - %s", stew.Version)
+        fmt.Printf("Stew Package Manager - %s\n", stew.Version)
 	},
 }
