fd2cc691e5063147d1494bf73ae238806bf9b852
Sting Alleman <stingalleman@icloud.com>
2021-04-19 19:54:22 +0200

    fix(main): handle error if syscall fails

--- main.go
+++ main.go
@@ -19,13 +19,15 @@ func main() {
 
 	// Check if running on Intel or Apple Silicon
 	r, err := syscall.Sysctl("sysctl.proc_translated")
-	if err != nil && err.Error() == "no such file or directory" {
-		config.GetConfig()
+	if err != nil && err.Error() == "no such file or directory" { // Intel
+		config.GetConfig() // Get config
 
 		cmd.Execute()
+	} else {
+		fmt.Printf("Unknown error while checking if Stew is running on Apple Silicon.\nError:%v\n", err)
 	}
 
-	if r == "\x00\x00\x00" || r == "\x01\x00\x00" {
+	if r == "\x00\x00\x00" || r == "\x01\x00\x00" { // Apple Silicon
 		fmt.Println("Looks like you're running Stew on Apple Silicon! We don't support this yet.")
 	}
 }
