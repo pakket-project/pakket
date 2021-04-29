af5eeeef72e981be15d8d39584f7e3c04299ee2e
Sting Alleman <stingalleman@icloud.com>
2021-04-29 11:38:13 +0100

    chore: remove rosetta/arm check

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"runtime"
-	"syscall"
 
 	"github.com/stewproject/stew/cmd"
 	"github.com/stewproject/stew/internals/config"
@@ -17,17 +16,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Check if running on Intel or Apple Silicon
-	r, err := syscall.Sysctl("sysctl.proc_translated")
-	if err != nil && err.Error() == "no such file or directory" { // Intel
-		config.GetConfig() // Get config
+	config.GetConfig() // Get config
 
-		cmd.Execute()
-	} else {
-		fmt.Printf("Unknown error while checking if Stew is running on Apple Silicon.\nError:%v\n", err)
-	}
-
-	if r == "\x00\x00\x00" || r == "\x01\x00\x00" { // Apple Silicon
-		fmt.Println("Looks like you're running Stew on Apple Silicon! We don't support this yet.")
-	}
+	cmd.Execute()
 }
