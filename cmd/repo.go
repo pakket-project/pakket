c05c3b68d140c6832753f5b78c39c4352d4decb3
Stitch07 <soumildiscord@gmail.com>
2021-05-03 09:56:24 +0530

    fix: apply requested changes

--- cmd/repo.go
+++ cmd/repo.go
@@ -34,25 +34,21 @@ var addCmd = &cobra.Command{
 	Example: "stew repo add https://github.com/stewproject/packages https://github.com/stingalleman/stew-repository",
 	Run: func(cmd *cobra.Command, args []string) {
 		spinner, _ := yacspin.New(util.SpinnerConf)
-
-		_, err := os.Stat(util.RepoPath)
-		if err != nil {
-			if os.IsNotExist(err) {
-				fmt.Printf("Repo path %s was not found: creating it\n ", util.RepoPath)
-				err := os.Mkdir(util.RepoPath, fs.ModeDir)
-				if err != nil {
-					fmt.Printf("failed to create %s: %s\n", util.RepoPath, style.Error.Render(err.Error()))
-					os.Exit(1)
-				}
-			} else {
-				// other stat errors
-				fmt.Printf("failed to stat %s: %s\n", util.RepoPath, style.Error.Render(err.Error()))
-				os.Exit(1)
-			}
-		}
-
 		spinner.Start()
 		spinner.Message("Adding repositories...")
+
+		_, err := os.Stat(util.RepoPath)
+		if os.IsNotExist(err) {
+			err := os.Mkdir(util.RepoPath, fs.ModeDir)
+			if err != nil {
+				fmt.Printf("failed to create %s: %s\n", util.RepoPath, style.Error.Render(err.Error()))
+				os.Exit(1)
+			}
+		} else if err != nil {
+			// other stat errors
+			fmt.Printf("failed to stat %s: %s\n", util.RepoPath, style.Error.Render(err.Error()))
+			os.Exit(1)
+		}
 
 		var addedRepos []string
 		for i := range args {
