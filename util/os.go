c8ecb8a0326a97f37337ca1032b73e73800b8846
Sting Alleman <stingalleman@icloud.com>
2021-05-21 08:25:43 +0100

    feat(util/os): add getVersion method

--- util/os.go
+++ util/os.go
@@ -2,19 +2,20 @@ package util
 
 import (
 	"os"
+	"os/exec"
+	"path"
+	"strings"
 )
 
 var (
 	// Path that houses config & repositores
-	StewPath = "/usr/local/stew"
+	StewPath = "/etc/stew"
 	// Path to main Stew config
-	ConfigFile = "/usr/local/stew/config.toml"
+	ConfigFile = path.Join(StewPath, "config.toml")
 	// Path to Stew repositories
-	RepoPath = "/usr/local/stew/repositories"
-	// Path to binary installation path
-	Binpath = "/usr/local/bin"
-	// Path to temporary clone repositories
-	TempRepoPath = "/var/tmp/stew/repo"
+	RepoPath = path.Join(StewPath, "repositories")
+	// Path to temporary downloads/clones
+	TempRepoPath = path.Join(StewPath, "downloads")
 )
 
 // Check if path exists
@@ -23,4 +24,23 @@ func DoesPathExist(path string) bool {
 	exists := os.IsNotExist(err)
 
 	return !exists
+}
+
+// Get macOS version.
+func GetVersion() (ver string) {
+	out, err := exec.Command("sw_vers", "-productVersion").Output()
+	if err != nil {
+		panic(err)
+	}
+	ver = string(out)
+
+	if strings.HasPrefix(ver, "11") {
+		return "big_sur"
+	} else if strings.HasPrefix(ver, "10.15") {
+		return "catalina"
+	} else if strings.HasPrefix(ver, "10.14") {
+		return "mojave"
+	} else {
+		panic("unsupported macOS version")
+	}
 }
