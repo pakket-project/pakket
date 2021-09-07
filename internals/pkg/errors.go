179daeccf462296cebd5d34219b330a88dd96e54
Shikhar Vashistha <shikharvashistha@yandex.com>
2021-09-07 05:53:00 +0000

    [Refactoring] hash -> checksum

--- internals/pkg/errors.go
+++ internals/pkg/errors.go
@@ -24,10 +24,10 @@ func (err VersionNotFoundError) Error() string {
 }
 
 // invalid sha256 hash (thrown if remote hash differs from repository hash)
-type InvalidHash struct {
+type InvalidChecksum struct {
 	Repository string
 }
 
-func (err InvalidHash) Error() string {
+func (err InvalidChecksum) Error() string {
 	return fmt.Sprintf("Cannot validate checksum. It is possible somebody has tampered with the file, or that you are the victim of a MITM-attack.\nContact the repository maintainer if you believe this is an error. (%s)", style.Repo.Render(err.Repository))
 }
