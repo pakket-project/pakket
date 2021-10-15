5c40ae4b75abb17e47d5f1ab98499682f1fac66f
Sting Alleman <stingalleman@icloud.com>
2021-10-15 21:06:39 +0200

    fix: remove hardcoded locations

--- internals/pkg/management.go
+++ internals/pkg/management.go
@@ -15,39 +15,6 @@ import (
 	"github.com/pakket-project/pakket/util"
 )
 
-// Manuals
-// 1 - General commands
-// 2 - System calls
-// 3 - Subroutines
-// 4 - Special files
-// 5 - File formats
-// 6 - Games
-// 7 - Macros and conventions
-// 8 - Maintenance commands
-// 9 - Kernel interface
-// n - New commands
-var (
-	locations = map[string]string{
-		// Binaries
-		"/bin":   "/usr/local/bin",
-		"/share": "/usr/local/share",
-		// Completions
-		// "/completions/fish": "",
-		// "/completions/zsh":  "/usr/share/zsh/5.3/functions",
-		// Manuals
-		// "/man/man1": "/usr/local/share/man/man1",
-		// "/man/man2": "/usr/local/share/man/man2",
-		// "/man/man3": "/usr/local/share/man/man3",
-		// "/man/man4": "/usr/local/share/man/man4",
-		// "/man/man5": "/usr/local/share/man/man5",
-		// "/man/man6": "/usr/local/share/man/man6",
-		// "/man/man7": "/usr/local/share/man/man7",
-		// "/man/man8": "/usr/local/share/man/man8",
-		// "/man/man9": "/usr/local/share/man/man9",
-		// "/man/mann": "/usr/local/share/man/mann",
-	}
-)
-
 // download and unarchive package.
 func DownloadPackage(pkg PkgData, savePath string) (err error) {
 	err = os.MkdirAll(util.DownloadPath, 0770)
@@ -118,35 +85,13 @@ func InstallPackage(pkg PkgData, force bool, yes bool) (err error) {
 			return err
 		}
 
-		// if file is in /share directory
-		if strings.Split(localPath, "/")[1] == "share" {
-			if f.IsDir() {
-				return err
-			}
-
-			filesToMove[filePath] = path.Join("/", "usr", "local", localPath)
+		// if path is a dir, return
+		if f.IsDir() {
 			return err
 		}
 
-		// if path is a file, return
-		if !f.IsDir() {
-			return err
-		}
+		filesToMove[filePath] = path.Join("/", "usr", "local", localPath)
 
-		if finalPath, ok := locations[localPath]; ok {
-			err = filepath.Walk(filePath, func(secondPath string, f os.FileInfo, err error) error {
-				if secondPath == filePath {
-					return err
-				}
-
-				filesToMove[secondPath] = path.Join(finalPath, f.Name())
-
-				return err
-			})
-			if err != nil {
-				return err
-			}
-		}
 		return err
 	})
 	if err != nil {
